1ce57c54e51632fbb75c7b8af818b28b67ec5688
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-23 12:09:51 -0500

    user model schema set up

--- models/user.js
+++ models/user.js
@@ -1,17 +1,38 @@
-//this is the simplest version of this model -- there can be much more
-//like DOB, address, phone number, etc
-
 const mongoose = require("mongoose");
+
+const applicationsSchema = new.mongoose.Schema({
+  company: {
+    type: String,
+    reqired: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing','rejected','accepted'],
+    required: true,
+  },
+});
+
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true, // calling this "validation". Prevents database from acceptin oopsing / backend validation 
+    required: true, 
   },
   password: {
     type: String,
     required: true,
   },
+  applications: [applicationsSchema],
 });
 
 const User = mongoose.model("User", userSchema);
