f726bb5749affaa28b7f528238cda5688192c440
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-24 15:03:27 -0500

    delete functionality added

--- controllers/applications.js
+++ controllers/applications.js
@@ -36,13 +36,26 @@ router.post('/', async (req, res) => {
 
 
 //GET to /applications/:applicationID
-router.get('/:applicationId', async (req,res) => {
+router.get('/:applicationId', async (req, res) => {
     try {
-        const currentUser = await User.findById(req.session.user._id); 
+        const currentUser = await User.findById(req.session.user._id);
         const application = currentUser.applications.id(req.params.applicationId);
-        res.render('applications/show.ejs',{
+        res.render('applications/show.ejs', {
             application: application,
         });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+//DELETE
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.id(req.params.applicationId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
     } catch (error) {
         console.log(error);
         res.redirect('/');
