ae8765afc39d775b7127cc68a238212e956dfea7
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-24 13:57:24 -0500

    data adding and displyaing for the user on the app index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,30 +3,34 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) =>{
+router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs');
+        const currentUser = await User.findById(req.session.user._id);
+
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications,
+        });
     } catch (error) {
         console.log(error); // keep, never show for the user, for data gathering 
         //note it is also ABOVE the response, if below wouldn't show
         res.redirect('/');
     }
-})
+});
 
-router.get('/new', async (req,res) => {
+router.get('/new', async (req, res) => {
     res.render('applications/new.ejs');
 });
 
 //POST for '/applications
-router.post('/', async (req,res) => {
+router.post('/', async (req, res) => {
     try {
-    const currentUser = await User.findById(req.session.user._id);     // Look up the user from req.session
-    currentUser.applications.push(req.body);  // Push req.body (the new form data object) to the applications array of the current user
-    await currentUser.save(); // Save changes to the user
-    res.redirect(`/users/${currentUser._id}/applications`);
-} catch (error) { //same as above
-      console.log(error); // If any errors, log them and redirect back home
-      res.redirect('/');  // Redirect back to the applications index view
+        const currentUser = await User.findById(req.session.user._id);     // Look up the user from req.session
+        currentUser.applications.push(req.body);  // Push req.body (the new form data object) to the applications array of the current user
+        await currentUser.save(); // Save changes to the user
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) { //same as above
+        console.log(error); // If any errors, log them and redirect back home
+        res.redirect('/');  // Redirect back to the applications index view
     }
 });
 
