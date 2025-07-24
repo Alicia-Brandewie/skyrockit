c56717f1fb36ee241142343cfd9ba9ca47b532c4
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-24 12:32:11 -0500

    add application form created but not sending data to database

--- controllers/applications.js
+++ controllers/applications.js
@@ -13,9 +13,21 @@ router.get('/', (req, res) =>{
     }
 })
 
-router.get('/new', async (req,res) =>{
+router.get('/new', async (req,res) => {
     res.render('applications/new.ejs');
-})
+});
 
+//POST for '/applications
+router.post('/', async (req,res) => {
+    try {
+    const currentUser = await User.findById(req.session.user._id);     // Look up the user from req.session
+    currentUser.applications.push(req.body);  // Push req.body (the new form data object) to the applications array of the current user
+    await currentUser.save(); // Save changes to the user
+    res.redirect(`/users/${currentUser._id}/applications`);
+} catch (error) { //same as above
+      console.log(error); // If any errors, log them and redirect back home
+      res.redirect('/');  // Redirect back to the applications index view
+    }
+});
 
 module.exports = router; 
